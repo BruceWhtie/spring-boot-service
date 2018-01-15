ce230f522debf2e83e18afacae739c5c90a0dbb2
EwingTsai <191237878@qq.com>
2018-01-16 00:31:50 +0800

    界面效果优化

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,20 +1,16 @@
 // 设置命名空间
 var Common = {
     /**
-     * 简单遍历树的节点，父节点会比子节点先访问
+     * 递归遍历树的节点，符合前序遍历顺序
      */
     traverseTree: function (treeNodes, callback) {
         if (treeNodes && treeNodes.length) {
-            var stack = [treeNodes];
-            while (stack.length) {
-                var nodes = stack.pop();
-                nodes.forEach(function (node) {
-                    callback(node);
-                    if (node.children) {
-                        stack.push(node.children);
-                    }
-                });
-            }
+            treeNodes.forEach(function (node) {
+                callback(node);
+                if (node.children) {
+                    Common.traverseTree(node.children, callback);
+                }
+            });
         }
     }
 };
